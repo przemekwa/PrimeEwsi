26f9a4b0060c572dfee9e85adcebf942a3956d9d
przemekwa <przemyslaw.walkowski@bzwbk.pl>
2016-09-29 16:29:59 +0200

    Dodanie fontawsome

---
+++ PrimeEwsi/PackApi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using UrbanCodeMetaFileCreator;
+
+namespace PrimeEwsi
+{
+    public class PackApi
+    {
+        public PrimeEwsiContext PrimeEwsiContext { get; set; }
+
+        public UrbanCodeMetaFIleApi UrbanCodeMetaFIleApi { get; set; }
+
+        public PackApi(PrimeEwsiContext primeEwsiContext, UrbanCodeMetaFIleApi urbanCodeMetaFIleApi)
+        {
+            PrimeEwsiContext = primeEwsiContext;
+            UrbanCodeMetaFIleApi = urbanCodeMetaFIleApi;
+        }
+
+      
+
+    }
+}
